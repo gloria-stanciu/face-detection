105834a0c224d872deb3c0c3526fd743991c5f9d
Gloria Stanciu <stanciugloria@gmail.com>
2023-01-24 21:10:23 +0200

    Delete webview html and store user id in the store

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { useGlobalStore } from './hooks/useGlobalStore'
+
+window.addEventListener('message', e => {
+  if (e?.data?.data?.user) {
+    useGlobalStore.setState(state => {
+      state.user.conversation_id = e?.data?.data?.user?.id
+    })
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
